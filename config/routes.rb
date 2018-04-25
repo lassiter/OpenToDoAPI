8d3c3061a27442ea3b200709a0c3356b4db51cc6
Lassiter Gregg <lassitergregg@gmail.com>
2018-04-24 21:15:46 -0500

    added User Auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    confirmations: 'users/confirmations',
-    omniauthable: 'users/omniauth_callbacks',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    unlocks: 'users/unlocks'
-  }
 
-  resources :users
+  
+  namespace :api, defaults: { format: :json } do
+    resources :users
+    resources :sessions, only: [:create, :destroy]
+  end
 end
--- db/migrate/20180424212252_create_users.rb
+++ db/migrate/20180424212252_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :email, null: false
+      t.string :password, null: false
       
       t.timestamps null: false
     end
